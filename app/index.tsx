d40d393dba22bc8414fb24a00e1ac05e0ef6f284
Isaac Pontes <isaaclfp@outlook>
2025-04-24 14:40:57 -0300

    Aula concluída: FlatList e ScrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Image, StatusBar } from 'react-native';
+import { View, Text, Image, StatusBar, FlatList } from 'react-native';
 import { styles } from '@/styles/globals';
 import { PrimaryButton } from '@/components/PrimaryButton';
 import { TransactionModal } from '@/components/TransactionModal';
@@ -9,6 +9,10 @@ const transactions = [
   { id: '2', description: 'Salário', amount: 2500.00 },
   { id: '3', description: 'Restaurante', amount: -120.40 },
   { id: '4', description: 'Aluguel', amount: -800.00 },
+  { id: '5', description: 'Ração do cachorro', amount: -199.99 },
+  { id: '6', description: 'Ida ao cinema', amount: -54.78 },
+  { id: '7', description: 'Freela', amount: 1600.00 },
+  { id: '8', description: 'Contas de luz', amount: -252.91 },
 ];
 
 const HomeScreen = () => {
@@ -41,23 +45,28 @@ const HomeScreen = () => {
         visible={modalVisible}
         onClose={() => setModalVisible(false)}
         onSave={(data) => {
-          alert(`Receita adicionada:\n\n ${JSON.stringify(data)}`); 
+          alert(`Receita adicionada:\n\n ${JSON.stringify(data)}`);
         }}
       />
 
       <Text style={styles.sectionTitle}>
         Transações Recentes
       </Text>
-      {transactions.map(transaction => (
-        <View key={transaction.id} style={styles.transactionItem}>
-          <Text style={styles.transactionText}>
-            {transaction.description}
-          </Text>
-          <Text style={[styles.transactionAmount, transaction.amount >= 0 ? styles.income : styles.expense]}>
-            R$ {transaction.amount.toFixed(2)}
-          </Text>
-        </View>
-      ))}
+
+      <FlatList
+        data={transactions}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.transactionItem}>
+            <Text style={styles.transactionText}>
+              {item.description}
+            </Text>
+            <Text style={[styles.transactionAmount, item.amount >= 0 ? styles.income : styles.expense]}>
+              R$ {item.amount.toFixed(2)}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
